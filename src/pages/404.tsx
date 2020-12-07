def05320470a19bc824317f325641122e66fea98
alayraza <50444174+alayraza@users.noreply.github.com>
2020-12-08 03:41:17 +0500

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { gql, useMutation} from "@apollo/client";
+
+const GETSpecificLolly = gql`
+    mutation SpecificlolliInfo($lollyPath: String!){
+        SpecificlolliInfo(lollyPath: $lollyPath){
+            recipientName
+            message
+            senderName
+            flavourTop
+            flavourMiddle
+            flavourBottom
+            lollyPath
+        }
+    }
+`
 
 const Page404 = ({ location }) => {
-  const path = location.pathname.slice(1, 7);
-  const slug = location.pathname.slice(7);
+    const [Url, setUrl] = useState("");
+    const [isData, setData] = useState("");
+    const isBrowser = typeof location !== `undefined`;
 
-  console.log("path ", path);
-  console.log("slug ", slug);
+    if(isBrowser){
+        const url=location.pathname.split("/");
+        const [SpecificlolliInfo] = useMutation(GETSpecificLolly);
+        const Spresult = SpecificlolliInfo({
+            variables : {
+                lollyPath:url[url.length -1]
+            }
+        }).then(value => {
+            if(url[url.length -1]!=Url){
+                setUrl(url[url.length -1]);
+            }
+            setData("done");
+            // console.log("specific data ", value.data.SpecificlolliInfo.flavourTop);
+        }).catch(function(err){
+            setData("error");
+            console.log("err ", err.toString());
+        });
+        // console.log("data ",StateflavourTop);
+    }    
+
   return (
     <div>
-     404
+        {isData=="error"?("Page Not Found"):"loading"}
     </div>
   );
 };
